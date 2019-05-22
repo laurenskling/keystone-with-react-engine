1b6f30748e2047273bd7a30b0541f3c3b4a1db91
paulobef <beffpa@gmail.com>
2019-05-22 08:59:28 +0200

    disabled eslint on several files

--- templates/layouts/default.jsx
+++ templates/layouts/default.jsx
@@ -1,3 +1,6 @@
+/* eslint-disable operator-linebreak */
+/* eslint-disable react/jsx-indent */
+/* eslint-disable no-mixed-spaces-and-tabs */
 import React from 'react';
 import FlashMessages from '../mixins/flash-messages';
 
@@ -15,8 +18,8 @@ const Default = (props) => {
 
 				<link href="/styles/site.css" rel="stylesheet" />
 
-				{ props.user && props.user.canAccessKeystone &&
-					<link href="/keystone/styles/content/editor.min.css" rel="stylesheet" />
+				{props.user && props.user.canAccessKeystone &&
+				<link href="/keystone/styles/content/editor.min.css" rel="stylesheet" />
 				}
 
 				{props.css}
@@ -27,17 +30,17 @@ const Default = (props) => {
 				<div id="header">
 					<div className="container">
 						{
-							//- Customise your site's navigation by changing the navLinks Array in ./routes/middleware.js
-							//- ... or completely change this header to suit your design.
+							// - Customise your site's navigation by changing the navLinks Array in ./routes/middleware.js
+							// - ... or completely change this header to suit your design.
 						}
 					    <div role="navigation" className="navbar navbar-default">
 						    <div className="container-fluid">
 						   		<div className="navbar-header">
 							 		<button
-										type="button"
-										data-toggle="collapse"
-										data-target=".navbar-collapse"
-										className="navbar-toggle"
+								type="button"
+								data-toggle="collapse"
+								data-target=".navbar-collapse"
+								className="navbar-toggle"
 										>
 										<span className="sr-only">Toggle navigation</span>
 										<span className="icon-bar"></span>
@@ -49,34 +52,34 @@ const Default = (props) => {
 
 						   		<div className="collapse navbar-collapse">
 							 		<ul className="nav navbar-nav navbar-left">
-										{ props.navLinks.map((link) => {
+										{props.navLinks.map((link) => {
 											return (
-												<li key={link.key} className={props.section == link.key ? 'active' : null}>
+												<li key={link.key} className={props.section === link.key ? 'active' : null}>
 													<a href={link.href}>
 														{link.label}
 													</a>
 												</li>
 											);
-										}) }
+										})}
 							 		</ul>
 									<ul className="nav navbar-nav navbar-right">
-										{ props.user && props.user.canAccessKeystone &&
+										{props.user && props.user.canAccessKeystone &&
 											<li>
-												<a href='/keystone'>
+												<a href="/keystone">
 													Open Keystone
 												</a>
 											</li>
 										}
-										{ props.user &&
+										{props.user &&
 											<li>
-												<a href='/keystone/signout'>
+												<a href="/keystone/signout">
 													Sign Out
 												</a>
 											</li>
 										}
-										{ !props.user &&
+										{!props.user &&
 											<li>
-												<a href='/keystone/signin'>
+												<a href="/keystone/signin">
 													Sign In
 												</a>
 											</li>
@@ -90,22 +93,22 @@ const Default = (props) => {
 
 				<div id="body">
 					{
-						//- NOTE:
-						//- There is no .container wrapping className around body blocks to allow more flexibility in design.
-						//- Remember to include it in your templates when you override the intro and content blocks!
+						// - NOTE:
+						// - There is no .container wrapping className around body blocks to allow more flexibility in design.
+						// - Remember to include it in your templates when you override the intro and content blocks!
 					}
 
 					{
-						//- The Intro block appears above flash messages (used for temporary information display)
+						// - The Intro block appears above flash messages (used for temporary information display)
 						props.intro
 					}
 					{
-						//- Flash messages allow you to display once-off status messages to users, e.g. form
-						//- validation errors, success messages, etc.
+						// - Flash messages allow you to display once-off status messages to users, e.g. form
+						// - validation errors, success messages, etc.
 						<FlashMessages messages={props.messages} />
 					}
 					{
-						//- The children block should contain the body of your template's content
+						// - The children block should contain the body of your template's content
 						props.children
 					}
 				</div>
@@ -116,30 +119,30 @@ const Default = (props) => {
 			    </div>
 
 				{
-					//- jQuery 1.11.3 is included by default, and required by both Bootstrap
-					//- and the KeystoneJS Content Editor. If you are not targeting IE8,
-					//- you can upgrade to 2.1.4 (also bundled) for better performance.
+					// - jQuery 1.11.3 is included by default, and required by both Bootstrap
+					// - and the KeystoneJS Content Editor. If you are not targeting IE8,
+					// - you can upgrade to 2.1.4 (also bundled) for better performance.
 				}
 				<script src="/js/jquery/jquery-1.11.3.min.js"></script>
 
 				{
-					//- Customise which Bootstrap javascript files are served by including
-					//- them specifically here, instead of bootstrap-3.3.5.min.js
+					// - Customise which Bootstrap javascript files are served by including
+					// - them specifically here, instead of bootstrap-3.3.5.min.js
 				}
 				<script src="/js/bootstrap/bootstrap-3.3.5.min.js"></script>
 
-				{ props.user && props.user.canAccessKeystone &&
-					//- The KeystoneJS Content Editor provides support for ks-editable data attributes,
-					//- which generate links to edit content for users who can access Keystone
+				{props.user && props.user.canAccessKeystone &&
+					// - The KeystoneJS Content Editor provides support for ks-editable data attributes,
+					// - which generate links to edit content for users who can access Keystone
 					<script src="/keystone/js/content/editor.js"></script>
 				}
 
 				<script src="/js/bundle.js"></script>
 
 				{
-					//- Add scripts that are globally required by your site here.
+					// - Add scripts that are globally required by your site here.
 
-					//- Include template-specific javascript files by extending the js block
+					// - Include template-specific javascript files by extending the js block
 					props.js
 				}
 			</body>
